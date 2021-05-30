39ab40aa6b19d5c69d66158cda10c8fbc0077dea
BalkisAbbassi <blkabbassi@gmail.com>
2021-05-30 23:58:25 +0100

    first commit

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { AuthService } from './../services/auth.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(private authService:AuthService) { }
+
+  ngOnInit(): void {
+  }
+  onLogout(){
+    this.authService.logout();
+  }
+
+}
